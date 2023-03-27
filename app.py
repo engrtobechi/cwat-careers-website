1990bacb1639b899f32ba8de094c26737588be9e
Tobechi Ohaeri <tobechi.ohaeri@gmail.com>
2023-03-27 09:12:10 +0100

    Updated the webapp to add jobs page.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, url_for, flash, redirect, abo
 
 app = Flask(__name__)
 # Create a secret key for client browser sessions
-app.config['SECRET_KEY'] = 'eSQ92-2i23#As1985/1'
+app.config['SECRET_KEY'] = 'add your key here'
 
 def get_db_connection():
     # Open connection to the database.db
@@ -12,12 +12,7 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-
-
-# Definning our route
-@app.route("/")
-def hello_world():
-
+def fetch_jobs():
     # Call the get_db_connection method we defined above
     conn = get_db_connection()
 
@@ -26,13 +21,48 @@ def hello_world():
     # Close the database connection
     conn.close()
 
+    return jobs_fetched
+
+# Definning our route
+@app.route("/")
+def index():
+
+    jobs_fetched = fetch_jobs()
+
     # Using the render_template function from flask we render a html template as well as pass in data
-    return render_template("home.html", jobs=jobs_fetched)
+    return render_template("index.html", jobs=jobs_fetched)
 
 # Let's create our portal api
-@app.route("/api/jobs")
-def list_jobs():
-    return jsonify(JOBS)
+@app.route("/api/jobs/<id>/")
+def jsonify_jobs(id):
+
+    jobs_fetched = fetch_jobs()
+
+    # Create a list to hold jobs which will be pass in rows to the jsonify function
+    json_list = []
+    for job in jobs_fetched:
+        json_list.append(job)
+
+    if int(id)-1 > len(jobs_fetched):
+        return jsonify({"message":"Not found"})
+    else:
+        return jsonify(dict(json_list[int(id)-1]))
+
+# Let's create a route for generating dynamic pages for each job
+@app.route("/jobs/<id>/")
+def jobs_item_page(id):
+
+    jobs_fetched = fetch_jobs()
+
+    # Create a list to hold jobs which will be pass in rows to the jsonify function
+    job_list = []
+    for job in jobs_fetched:
+        job_list.append(job)
+
+    if int(id)-1 > len(jobs_fetched):
+        return "Not found", 404
+    else:
+        return render_template("jobpage.html", job=job_list[int(id)-1])
 
 # This block of code ensures that app.py starts the server when run
 if __name__ == "__main__":
